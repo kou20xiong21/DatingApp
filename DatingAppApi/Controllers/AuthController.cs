451561473242f52c3d58350938f6d903d07ec291
kou20xiong21 <kou20.xiong21@gmail.com>
2021-08-12 15:35:59 -0400

    initial Changes

---
+++ DatingAppApi/Controllers/AuthController.cs
@@ -0,0 +1,88 @@
+﻿using DatingAppApi.Data;
+using DatingAppApi.Dtos;
+using DatingAppApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace DatingAppApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthRepository _Repo;
+
+        public AuthController(IAuthRepository _repo)
+        {
+            _Repo = _repo;
+        }
+
+        // GET: AuthController
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserForRegisterDto userForRegisterDto)
+        {
+
+            userForRegisterDto.UserName = userForRegisterDto.UserName.ToLower();
+
+            if (await _Repo.UserExists(userForRegisterDto.UserName))
+            {
+                ModelState.AddModelError("UserName", "Username is already taken!");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userToCreate = new User
+            {
+                UserName = userForRegisterDto.UserName
+            };
+
+            var createUser = await _Repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserFromLoginDto userFromLoginDto)
+        {
+            var userFromRepo = await _Repo.Login(userFromLoginDto.Username, userFromLoginDto.Password);
+
+            if (userFromLoginDto == null)
+            {
+                return Unauthorized();
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("Super Secret Key");
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                    new Claim(ClaimTypes.Name, userFromRepo.UserName)
+                }),
+
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { tokenString });
+        }
+    }
+}
+
--- DatingAppApi/Migrations/20210812144054_AddedUserModel.Designer.cs
+++ DatingAppApi/Migrations/20210812144054_AddedUserModel.Designer.cs
@@ -4,14 +4,16 @@ using DatingAppApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingAppApi.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210812144054_AddedUserModel")]
+    partial class AddedUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
